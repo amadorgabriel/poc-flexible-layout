6dbacde78595ee355ba566b728351d4a6c8bd94c
amadorgabriel <gabrielramador2014@gmail.com>
2023-07-12 16:02:13 -0300

    feat: add allotment pane

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import { Allotment } from "allotment";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Preview } from "@/components/Preview";
+import { DetailsBar } from "@/components/DetailsBar";
+import { ComponentsBar } from "@/components/ComponentsBar";
+
+import "allotment/dist/style.css";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -13,9 +20,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <h1>Flexible Workspace</h1>
+
+      <main className={`${styles.container} ${inter.className}`}>
+        <Allotment minSize={100}>
+          <Allotment.Pane maxSize={400} minSize={200}>
+            <ComponentsBar />
+          </Allotment.Pane>
+
+          <Allotment.Pane>
+            <Preview />
+          </Allotment.Pane>
+
+          <Allotment.Pane maxSize={400} minSize={200}>
+            <DetailsBar />
+          </Allotment.Pane>
+        </Allotment>
       </main>
     </>
-  )
+  );
 }
