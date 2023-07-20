af28d617cd8f32ae162fa4a19c97e76887f51a97
amadorgabriel <gabrielramador2014@gmail.com>
2023-07-20 11:18:58 -0300

    feat: init moveable project

--- src/components/Preview/Node.component.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/Preview.module.css";
-
-export const Node = ({ text }: any) => {
-  return (
-    <div className={styles.gridWrapper}>
-      <div className={styles.gridItem}>{text}</div>
-    </div>
-  );
-};
