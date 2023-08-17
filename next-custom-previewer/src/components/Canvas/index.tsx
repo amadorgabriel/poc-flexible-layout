0bcfa3f7d0e2b5e118d6a01176e02ee90ddbbcdb
amadorgabriel <gabrielramador2014@gmail.com>
2023-08-16 23:34:12 -0300

    feat: add infinite viewer

--- next-custom-previewer/src/components/Canvas/index.tsx
+++ next-custom-previewer/src/components/Canvas/index.tsx
@@ -1,26 +1,47 @@
+import { useRef, useState } from "react";
 import { Page } from "./Page";
-
+import InfiniteViewer from "react-infinite-viewer";
 import { useLabelContext } from "@/core/contexts/LabelContext";
 
 export const Canvas = () => {
   const { pages, contentGroups } = useLabelContext();
-  
+
+  const [zoom, setZoom] = useState<number>();
+
+  const infiniteViewerRef = useRef<any>();
+
   return (
     <div className="canvas">
-      {pages.map((page, index) => {
-        const contents = contentGroups.filter(
-          (item) => item.pageId === page.id
-        );
+      <InfiniteViewer
+        ref={infiniteViewerRef}
+        usePinch={true}
+        pinchThreshold={50}
+        className="infinite-viewer"
+        style={{
+          width: `${infiniteViewerRef.current?.width + "px" || "100%"}`,
+        }}
+        zoom={zoom}
+        onPinch={(e) => {
+          setZoom(e.zoom);
+        }}
+      >
+        <div className="viewport">
+          {pages.map((page, index) => {
+            const contents = contentGroups.filter(
+              (item) => item.pageId === page.id
+            );
 
-        return (
-          <Page
-            key={index}
-            page={page}
-            pageContentGroups={contents}
-            hasAddBtn={pages.length === index + 1}
-          />
-        );
-      })}
+            return (
+              <Page
+                key={index}
+                page={page}
+                pageContentGroups={contents}
+                hasAddBtn={pages.length === index + 1}
+              />
+            );
+          })}
+        </div>
+      </InfiniteViewer>
     </div>
   );
 };
